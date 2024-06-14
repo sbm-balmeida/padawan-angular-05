386dd09aad11326942e490b6d3e7e71a290473d5
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-14 16:38:06 -0300

    Mostrando a lista de favoritos

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -23,8 +23,22 @@ export class PensamentoService {
       params = params.set("q", filtro)
     }
 
-    return this.http
-    .get<Pensamento[]>(this.API, { params: params })
+    return this.http.get<Pensamento[]>(this.API, { params: params })
+  }
+
+  listarPensamentosFavoritos(pagina: number, filtro: string): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set("_page", pagina)
+      .set("_limit", itensPorPagina)
+      .set("favorito", true)
+
+    if (filtro.trim().length > 2) {
+      params = params.set("q", filtro)
+    }
+
+    return this.http.get<Pensamento[]>(this.API, { params: params })
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {

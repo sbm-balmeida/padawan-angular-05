1629686fc67d75c6469fd72a3d171dcb05842059
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-14 11:32:37 -0300

    Habilitar/desabilitar botão

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -45,4 +45,12 @@ export class CriarPensamentoComponent implements OnInit {
     this.router.navigate(['/listarPensamento'])
   }
 
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
+  }
+
 }

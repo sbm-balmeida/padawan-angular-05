ff63113cb4999bfbd44583e3df4cfe2ee209c576
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-17 11:52:00 -0300

    Modificando o título

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -16,6 +16,7 @@ export class ListarPensamentoComponent implements OnInit {
   filtro: string = '';
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
   constructor(private service: PensamentoService, private router: Router) { }
 
@@ -30,7 +31,7 @@ export class ListarPensamentoComponent implements OnInit {
       .subscribe(listaPensamentos => {
         this.listaPensamentos.push(...listaPensamentos);
 
-        if (!this.listaPensamentos.length) {
+        if (!listaPensamentos.length) {
           this.haMaisPensamentos = false;
         }
       }
@@ -55,6 +56,7 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   listarFavoritos() {
+    this.titulo = 'Meus Favoritos'
     this.favoritos = true
     this.haMaisPensamentos = true
     this.paginaAtual = 1
